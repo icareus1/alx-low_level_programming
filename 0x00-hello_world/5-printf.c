84b5c96bdbaf927090ca1fdc795dc4a8320eafd5
icareus1 <wamars@live.fr>
2023-02-16 08:29:25 -0800

    Print a line using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - print a line with printf
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
