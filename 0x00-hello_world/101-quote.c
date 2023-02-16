c01483c4bf91b60142f257262790b083878f4c8d
icareus1 <wamars@live.fr>
2023-02-16 09:55:34 -0800

    Print a line without puts & printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - print a line of text
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
