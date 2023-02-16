454ed9b306ae7ba6830684de4dda093d6db23d3a
icareus1 <wamars@live.fr>
2023-02-16 08:16:48 -0800

    Print a line using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print a line with puts function
+ *
+ * return - 0 if successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
