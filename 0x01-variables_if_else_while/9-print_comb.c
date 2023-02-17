cd9aa4ea754461435401a17a14c9ca57af955650
icareus1 <wamars@live.fr>
2023-02-17 08:19:50 -0800

    Var,conditions,loops

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - print nums in lowercase
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch + '0');
+		putchar(' ');
+		putchar(',');
+	}
+		putchar('\n');
+	return (0);
+}
