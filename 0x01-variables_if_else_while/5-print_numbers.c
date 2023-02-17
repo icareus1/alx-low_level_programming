c617825447f68347dff1978f9fc9395098940e59
icareus1 <wamars@live.fr>
2023-02-17 07:53:38 -0800

    Var,conditions,loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print numbers up to 10
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		printf("%d", num);
+	}
+		putchar('\n');
+	return (0);
+}
