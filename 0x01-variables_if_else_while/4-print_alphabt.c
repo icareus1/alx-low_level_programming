808eecef4eecaaa0127f8d0500158a79d408ac56
icareus1 <wamars@live.fr>
2023-02-17 07:42:48 -0800

    Var,conditions,loops

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - print alphabets in lowercase
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' || ch != 'e')
+		{
+			putchar(tolower(ch));
+		}
+	}
+		putchar('\n');
+	return (0);
+}
