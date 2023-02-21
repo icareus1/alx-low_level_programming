c2a2d4ef500781198f72b32671edfde670a95a04
icareus1 <wamars@live.fr>
2023-02-21 13:05:24 -0800

    Functions

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,19 +5,18 @@
  */
 int main(void)
 {
-    int num1 = 1, num2 = 2, sum = 0, even_sum = 0;
+	int num1 = 1, num2 = 2, sum = 0, even_sum = 0;
 
-    while (num2 <= 4000000) {
-        sum = num1 + num2;
-        num1 = num2;
-        num2 = sum;
-
-        if (num1 % 2 == 0) {
-            even_sum += num1;
-        }
-    }
-
-    printf("%d\n", even_sum);
-
-    return 0;
+	while (num2 <= 4000000)
+	{
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+		if (num1 % 2 == 0)
+		{
+			even_sum += num1;
+		}
+	}
+	printf("%d\n", even_sum);
+	return 0;
 }
