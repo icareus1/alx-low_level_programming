9f35bdef0a556ba136cebd3a7987fd1cd6c1c17b
icareus1 <wamars@live.fr>
2023-02-21 07:16:02 -0800

    Functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i=0;
+	char ch;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++
+	}
+}
