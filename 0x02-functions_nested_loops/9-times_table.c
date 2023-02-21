c4533d3a46db01d0f3bb9b7cf61ec3107ae557d8
icareus1 <wamars@live.fr>
2023-02-21 09:54:54 -0800

    Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,16 +5,27 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, x;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			x = i*j;
+			if (x < 10 && x != 0)
+			{
+				_putchar(x + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			if (x >= 10)
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 			_putchar('\n');
 	}
