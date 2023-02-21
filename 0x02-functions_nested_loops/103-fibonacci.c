2c9fc664d17a5d6d037bbce6ad4bacab308b3a4c
icareus1 <wamars@live.fr>
2023-02-21 13:00:38 -0800

    Functions

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints sum of even fibnoacci nums under 4,000,000.
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int num1 = 1, num2 = 2, sum = 0, even_sum = 0;
+
+    while (num2 <= 4000000) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+
+        if (num1 % 2 == 0) {
+            even_sum += num1;
+        }
+    }
+
+    printf("%d\n", even_sum);
+
+    return 0;
+}
