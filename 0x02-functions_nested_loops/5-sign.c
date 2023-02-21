7fd2a5379a74c79644bdc10a8c58dca8ad5ab856
icareus1 <wamars@live.fr>
2023-02-21 08:16:03 -0800

    Functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - check number sign
+ * @n: number to check
+ * Return: 1 if successful, 0 if not
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
