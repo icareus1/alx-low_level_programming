e049d723d25fb50d80b5a6c9ac398774ed193ece
icareus1 <wamars@live.fr>
2023-02-21 06:51:46 -0800

    Functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+    for (int ch = 'a'; ch <= 'z'; i++)
+    {
+	 _putchar(ch);
+    }
+    _putchar('\n');
+    return (0);
+}
