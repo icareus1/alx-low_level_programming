3d418d3802818e10f6b615e8cba0695cd0e0b4b0
icareus1 <wamars@live.fr>
2023-02-21 06:11:06 -0800

    Functions in C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - use _putchar to print a simple statement
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
