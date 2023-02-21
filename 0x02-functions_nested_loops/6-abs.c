c4b2d42fd04f13435da2b64e8ef7a01c3d7dadaf
icareus1 <wamars@live.fr>
2023-02-21 08:21:36 -0800

    Functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - absolute value of a number
+ * @n: num to check
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >=0)
+		return (n);
+	else
+		return (-n);
+}
