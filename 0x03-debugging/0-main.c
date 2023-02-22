be8a7ddb4d530ac546b741320606357f1a194dfd
icareus1 <wamars@live.fr>
2023-02-22 11:52:19 -0800

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	n = 10;
+	positive_or_negative(n);
+	return (0);
+}
