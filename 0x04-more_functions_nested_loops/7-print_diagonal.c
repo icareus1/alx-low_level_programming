d995013a36306516d64cd142794df5fd0d72f7a0
icareus1 <wamars@live.fr>
2023-02-23 12:17:41 -0800

    More Functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,8 +13,14 @@ void print_diagonal(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('\\');
+			for (j = 0; j <= n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
