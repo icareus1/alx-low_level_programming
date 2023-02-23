10f51faa7eab899446c403b4c379703dadabb8c4
icareus1 <wamars@live.fr>
2023-02-23 12:28:10 -0800

    More Functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,12 +6,13 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
 	{
 		int i, j;
 
-		for (i = 1; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
