0535197090abf87d057e11ad6079026458e9204d
icareus1 <wamars@live.fr>
2023-02-23 10:16:19 -0800

    More Functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - checks for a digit (0 through 9).
+ */
+void print_numbers(void)
+{
+ 	int i;
+
+ 	for (i = 48; i < 58; i++)
+ 	{
+ 		_putchar(i);
+ 	}
+ 	_putchar('\n');
+}
