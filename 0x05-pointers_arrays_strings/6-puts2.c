9219d4032e84af9bb28b22918415c572029a5671
icareus1 <wamars@live.fr>
2023-02-27 13:35:19 -0800

    Pointers, Strings, Array

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - print only one character out of two starting with the first one
+ * @str: input
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	char *s = str;
+	int i;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	length–;
+	for (i = 0 ; i <= length ; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
