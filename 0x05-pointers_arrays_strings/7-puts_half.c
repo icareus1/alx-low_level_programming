1371bc09dd1aa7df9ceab8e954a557461eb5c29e
icareus1 <wamars@live.fr>
2023-02-27 13:52:23 -0800

    Pointers, Strings, Array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 		n = ((length - 1) / 2);
 	else
 		n = (length / 2);
-	for (i = n; str[i] != "\0"; i++)
+	for (i = n; *str != "\0"; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
