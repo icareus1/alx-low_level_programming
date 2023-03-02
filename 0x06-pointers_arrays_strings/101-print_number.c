6467a69652ea4fa80b6493a91ac8b6ca0e328b9e
icareus1 <wamars@live.fr>
2023-03-02 05:55:08 -0800

    More Pointers, Strings, Array

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - print numbers characters
+ * @n: integer value
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
