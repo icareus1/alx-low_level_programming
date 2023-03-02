cc1bad6f7f983f352c6f3310a35e163d3e4e71c6
icareus1 <wamars@live.fr>
2023-03-02 05:29:55 -0800

    More Pointers, Strings, Array

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encrypt the string using rot13 (rotate 13)
+ * @s: string value
+ * Return: the changed string
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char s1[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char s2[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
