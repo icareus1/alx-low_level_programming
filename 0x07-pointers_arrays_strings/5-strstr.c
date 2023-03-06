45f21bccbd55c3f11755494112caf6046bdb88a2
icareus1 <wamars@live.fr>
2023-03-06 14:34:20 -0800

    Even More Pointers, Strings, Array

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - return a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *str = haystack;
+		char *p = needle;
+
+		while (*str == *p && *p != '\0')
+		{
+			str++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+}
