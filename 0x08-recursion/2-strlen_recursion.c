f8cd7103d1c354b7e07154cde9a29711c089ebb6
icareus1 <wamars@live.fr>
2023-03-08 11:34:25 -0800

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - string length
+ * @s: string
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*s)
+	{
+		i++;
+		_strlen_recursion(s);
+		return (i);
+	}
+}
