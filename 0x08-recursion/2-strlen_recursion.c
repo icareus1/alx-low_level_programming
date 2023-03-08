04a94729af4e5b61e311467c3dfe121f3893fb9f
icareus1 <wamars@live.fr>
2023-03-08 11:45:55 -0800

    Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	if (*s)
 	{
-		i++;
-		_strlen_recursion(s + 1);
-		return (i);
+		i += _strlen_recursion(s + 1);
 	}
+	return (i);
 }
