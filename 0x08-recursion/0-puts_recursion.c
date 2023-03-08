f43434121ebf618b89bf79a601d3853631439b9c
icareus1 <wamars@live.fr>
2023-03-08 05:34:00 -0800

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts() and prints string;
+ * @s: the string
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] != “\0”)
+	{
+		_putchar(s[i]);
+		i++;
+		_puts_recursion(s[i]);
+	}
+
+	else
+		_putchar('\n');
+}
