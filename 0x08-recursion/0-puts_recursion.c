9a47dff83964744a0e841452b2d7483b61488fea
icareus1 <wamars@live.fr>
2023-03-08 05:41:07 -0800

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (s[i] != "\0")
+	if (*s != "\0")
 	{
-		_putchar(s[i]);
-		i++;
-		_puts_recursion(s[i]);
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
 
 	else
