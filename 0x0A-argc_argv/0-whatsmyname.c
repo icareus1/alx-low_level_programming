a199cc8f95516922f9e42816a8cfb484c075398b
icareus1 <wamars@live.fr>
2023-03-10 13:07:58 -0800

    Args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: string of arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
