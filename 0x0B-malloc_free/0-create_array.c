22077d470c3247e4638757b6b2c2b457711c4a40
icareus1 <wamars@live.fr>
2023-03-13 11:38:43 -0700

    Malloc, Free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	s = malloc(sizeof(char) * size);
 	if (size == 0)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i<size; i++)
 		s[i] = c;
 	return (s);
 }
