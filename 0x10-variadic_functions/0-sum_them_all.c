722629c564b5ac5a2ed0d6f98cafea2b5272b3a0
icareus1 <wamars@live.fr>
2023-03-23 09:20:06 -0700

    Variadic Function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include<stdarg.>
 #include "variadic_functions.h"
 /**
@@ -11,9 +10,6 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i, sum = 0;
-
-	if (n == 0)
-		return (0);
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
