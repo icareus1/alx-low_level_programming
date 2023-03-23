9d28e1d9808178bfe08d5a4e127cec79540a0255
icareus1 <wamars@live.fr>
2023-03-23 09:16:05 -0700

    Variadic Function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,9 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 	unsigned int i, sum = 0;
 
-	if (n == 0)
+/*	if (n == 0)
 		return (0);
-
+*/
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
