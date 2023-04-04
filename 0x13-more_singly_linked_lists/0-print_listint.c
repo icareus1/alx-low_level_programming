1e945dd2a03dfbfbd65bd7cda6f77b1d1d384f86
icareus1 <wamars@live.fr>
2023-04-04 09:48:50 -0700

    More Singly Linked List

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint -  prints all the elements of the list
+ * @h: contains the address of a node
+ * Return: number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while(h)
+	{
+		printf("%d", h->n);
+		count++;
+		h = h->next;
+	}
+	return(count);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+ /**
+  * listint_len -  prints number of elements in the list
+  * @h: contains the address of a node
+  * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while(h)
+	{
+		count++;
+		h = h->next;
+	}
+	return(count);
+}
