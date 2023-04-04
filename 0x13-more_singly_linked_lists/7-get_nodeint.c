aa425c8cf235b6a08f1ffc63c77bae572047f22e
icareus1 <wamars@live.fr>
2023-04-04 13:39:03 -0700

    More Singly Linked List

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - display a node at a certain index
+ * @head: contains the address of the first node
+ * @index: position of the node to be printed
+ * Return: NULL if node doesn't exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *currentnode = head;
+	int pos = 0;
+
+	while (currentnode)
+	{
+		if (pos == index)
+			return (currentnode);
+		pos++;
+		currentnode = currentnode->next;
+	}
+	return (NULL);
+}
